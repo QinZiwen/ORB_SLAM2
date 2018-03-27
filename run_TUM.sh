cd2de6437128beb2c16ef352bf10ddf84433a306
ZiwenQin <qinziwen2013@163.com>
2018-03-27 15:56:12 +0800

    add run_TUM.sh

---
+++ run_TUM.sh
@@ -0,0 +1,16 @@
+#########################################################################
+# File Name: run_RGBDtum.sh
+# Author: QinZiwen
+# mail: qinziwen2013@163.com
+# Created Time: 2017年06月07日 星期三 16时50分00秒
+#########################################################################
+#!/bin/bash
+
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_360/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_360/associated.txt
+./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_desk/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_desk/associated.txt
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_desk2/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_desk2/associated.txt
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_room/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg1_room/associated.txt
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_structure_notexture_far/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_structure_notexture_far/associated.txt
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_large_cabinet/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_large_cabinet/associated.txt
+# ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_nostructure_notexture_near_withloop/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_nostructure_notexture_near_withloop/associated.txt
+#./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_structure_texture_far/ /home/turtlebot/Database/TUM/rgbd_dataset_freiburg3_structure_texture_far/associated.txt
